505420b2f313292f800983382d37431bd8eed859
whdddd <148370313+tavin7xis@users.noreply.github.com>
2024-10-15 15:48:32 -0300

    efmkvmgtrk

--- app.js
+++ app.js
@@ -8,4 +8,17 @@ function criaCartao(categoria,pergunta,resposta){
     <h3>${categoria}</h3>
     <div class="cartao__conteudo__pergunta">
 }<p>${pergunta}</p>
+</div> 
+<div class=cartao__conteudo__resposta">
+<p>${resposta}</p>
 </div>
+</div>
+``
+let respostaEstaVisivel= false
+
+function viraCartao(){
+    respostaEstaVisivel=respostaEstaVisivel
+    cartao.classList.toggle('activate'  respostaEstaVisivel)
+    }
+    cartao.addEventListener('click'viraCartao)
+    container.appendChild(cartao)
